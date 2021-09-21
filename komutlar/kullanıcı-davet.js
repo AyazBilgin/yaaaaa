9d2689e1249030a912aefff2913d79f8cb7bbf12
Glitch (heavy-nine-palladium) <none>
2021-09-21 17:32:25 +0000

    🐗📟 Checkpoint ./komutlar/kullanıcı-istatistik.js:35716997/367 ./komutlar/kullanıcı-davet.js:35716997/306

--- komutlar/kullanıcı-davet.js
+++ komutlar/kullanıcı-davet.js
@@ -5,11 +5,11 @@ exports.run = (client, message) => {
   const davet = new Discord.MessageEmbed()
   .setColor('#f6ff00')
   .setThumbnail('https://media.discordapp.net/attachments/720029424216440923/735255651638313061/727894683061321759.gif')
-  .setDescription(`** BRK Bot Davet Linki Ve Destek Sunucusu **
+  .setDescription(`** Fregas Bot Davet Linki Ve Destek Sunucusu **
 
-**[Bot Davet](https://discord.com/api/oauth2/authorize?client_id=877628391031988266&permissions=261993005055&scope=bot)**
-**[Destek Sunucusu](https://discord.gg/tahsineren)** 
-**[Bot Sitesi](https://brkbot.glitch.me)**
+**[Bot Davet](https://discord.com/oauth2/authorize?client_id=882636769147904030&scope=bot&permissions=805314622)**
+**[Destek Sunucusu](https://discord.gg/Dj5CF27Pfy)** 
+**[Bot Sitesi Yakında]**
 
 
 
--- komutlar/kullanıcı-istatistik.js
+++ komutlar/kullanıcı-istatistik.js
@@ -12,7 +12,7 @@ exports.run = async (client, message, args) => {
 .setThumbnail(message.author.displayAvatarURL({dynamic : true}))
   .setTitle('Üzgünüm Bot Bakımda')
   .addField('Bot Şuan Bakımdadır Lütfen Bekleyin.','Bot Ne Durumda Yada Botla İlgili Güncelleme Ve Duyurular İçin Destek Sunucumuza Gelmeyi Unutmayınız.')
-  .addField('İşte Destek Sunucum',"[Destek Sunucusu]()")
+  .addField('İşte Destek Sunucum',"[Destek Sunucusu](https://discord.gg/Dj5CF27Pfy)")
   .setFooter('Üzgünüm...')
   .setImage('https://lh3.googleusercontent.com/proxy/gAN4I19oqqabXd_VIiwg5or-ITh4XxJTRNJA1ot0EIHPiBpxC74Atj4wNIcFes1N3VcE8WnOk6fIN29BChqNbj4lj9dIF2jiI7MBV6U8v842LA')
 if(message.author.id != "627803211348312065") return message.channel.send(bakim)
@@ -21,14 +21,14 @@ if(message.author.id != "627803211348312065") return message.channel.send(bakim)
       	 let karaliste = db.fetch(`ckaraliste.${message.author.id}`)
  const westraben = new Discord.MessageEmbed()
  .setColor(`#f6ff00`)
- .setDescription(` **${karaliste}** sebebiyle karalisteye alınmışsın!\nBeyaz listeye alınmak istiyorsan [BURAYA](https://discord.gg/kqaBAxkkuX) gelebilirsin!`)
+ .setDescription(` **${karaliste}** sebebiyle karalisteye alınmışsın!\nBeyaz listeye alınmak istiyorsan [BURAYA](https://discord.gg/Dj5CF27Pfy) gelebilirsin!`)
   if(karaliste) 
     return message.channel.send(westraben)
   const msg = new Discord.MessageEmbed()
     .setColor("#f6ff00")
     .setFooter(client.user.tag, client.user.avatarURL())
   .addField(
-  "» **Sahibim**","<@751717933700349972>"
+  "» **Sahibim**","<@853209984510984205>"
   )
     .addField(
       "» **Bellek kullanımı**",
@@ -76,7 +76,7 @@ if(message.author.id != "627803211348312065") return message.channel.send(bakim)
     .setThumbnail(client.user.avatarURL())
   .setImage("")
     .addField("» **İşletim Sistemi**", `\`\`${os.platform()}\`\``)
-    .addField("**➥ Linkler**", "**[Davet Linki](https://discord.com/oauth2/authorize?client_id=869251332010299392&scope=bot&permissions=8)**\n **[Destek Sunucu](https://discord.gg/kqaBAxkkuX)**\n**[Website= YAKINDA]()**");
+    .addField("**➥ Linkler**", "**[Davet Linki](https://discord.com/oauth2/authorize?client_id=882636769147904030&scope=bot&permissions=805314622)**\n **[Destek Sunucu](https://discord.gg/Dj5CF27Pfy)**\n**[Website= YAKINDA]()**");
   return message.channel.send(msg);
 };
 
