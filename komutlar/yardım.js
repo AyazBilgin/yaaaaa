a2ac0b9f0dd0562ce1c4951c9c4dd74392091ad4
Glitch (heavy-nine-palladium) <none>
2021-09-21 15:09:14 +0000

    🍎😄 Checkpoint ./ayarlar.json:35716997/39 ./komutlar/yardım.js:35716997/474

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -17,7 +17,7 @@ exports.run = async (client, message, args) => {
 .setThumbnail(message.author.displayAvatarURL({dynamic : true}))
   .setTitle('Üzgünüm Bot Bakımda')
   .addField('Bot Şuan Bakımdadır Lütfen Bekleyin.','Bot Ne Durumda Yada Botla İlgili Güncelleme Ve Duyurular İçin Destek Sunucumuza Gelmeyi Unutmayınız.')
-  .addField('İşte Destek Sunucum',"[Destek Sunucusu](https://discord.gg/kqaBAxkkuX)")
+  .addField('İşte Destek Sunucum',"[Destek Sunucusu](https://discord.gg/jpCXm6HugQ)")
   .setFooter('Üzgünüm...')
   .setImage('https://lh3.googleusercontent.com/proxy/gAN4I19oqqabXd_VIiwg5or-ITh4XxJTRNJA1ot0EIHPiBpxC74Atj4wNIcFes1N3VcE8WnOk6fIN29BChqNbj4lj9dIF2jiI7MBV6U8v842LA')
 if(message.author.id != "627803211348312065") return message.channel.send(bakim)
@@ -30,8 +30,8 @@ let eklenti = new Discord.MessageEmbed()
 .setColor('#f6ff00')
 .addField( `**╔═══════════◥◣❖◢◤════════════╗**`,
     ` **║ Bilgilendirme.**
-**║ [Botu Davet Et](https://discord.com/api/oauth2/authorize?client_id=877628391031988266&permissions=261993005055&scope=bot)**
-**║ [Destek Sunucusu](https://discord.gg/f35ruDy8)**
+**║ [Botu Davet Et](https://discord.com/oauth2/authorize?client_id=882636769147904030&scope=bot&permissions=805314622)**
+**║ [Destek](https://discord.gg/jpCXm6HugQ)**
 **║ Bot Sitesi (Yakında)**
 **╚═══════════◥◣❖◢◤════════════╝**
    `)
