b7c9378fd0832e666cb3efe2cfdeaa6b2f106421
Glitch (heavy-nine-palladium) <none>
2021-09-24 16:43:33 +0000

    👮😋 Checkpoint ./events/ready.js:35671612/646

--- events/ready.js
+++ events/ready.js
@@ -17,17 +17,3 @@ var oyun = [
       "Sunucular",
       client.guilds.cache.size.toLocaleString(),
       true
-    
-
-    setInterval(function() {
-
-        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
-
-        client.user.setActivity(oyun[random], "https://discord.gg/Dj5CF27Pfy");
-        }, 2 * 20000);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] -----Aloysha Developers------`);
-  client.user.setStatus("idle");
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
-};
